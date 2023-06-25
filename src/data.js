55390e0c2bf899d311a16a6531761fb781a2ff5a
Avinoorsingh <avinoorsingh123@gmail.com>
2023-06-25 13:16:10 -0400

    products array file added

---
+++ src/data.js
@@ -0,0 +1,48 @@
+const data={
+    products:[
+        {
+            _id:"1",
+            name:"Nike Slim Shirt",
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            _id:"2",
+            name:"Nike Medium Shirt",
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            _id:"3",
+            name:"Nike Large Shirt",
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            _id:"4",
+            name:"Nike Extra-Large Shirt",
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+        },
+    ]
+}
